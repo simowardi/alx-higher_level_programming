da9c3a7b1a5f642fb7c5defee0bfb46934be5be7
simowardi <daoist32@gmail@gmail.com>
2024-01-08 10:18:27 -0800

    task 8 & 9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == '':
+        first = None
+        my_tup = len(sentence), first
+        return my_tubtup
+
+    my_tup = len(sentence), sentence[0]
+    return my_tup
