420fc0afc4fd4948b9dc9c8683a102da000a0788
simowardi <daoist32@gmail@gmail.com>
2024-03-12 16:46:59 -0700

    task 16 updt

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
 -- lists all records of the second_table
 -- Records are ordered by descending score.
-SELECT score, name FROM second_table WHERE NOT name='' ORDER BY score DESC;`
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
