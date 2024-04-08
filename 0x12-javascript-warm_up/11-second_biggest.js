e3c0e95206d54b31e55f3f2ccf0d75830135b60f
simowardi <daoist32@gmail@gmail.com>
2024-04-08 10:55:17 -0700

    javascript basics

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const numbers = process.argv.slice(2).map(Number);
+
+if (numbers.length <= 1) {
+  console.log(0);
+} else {
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
+}
+
