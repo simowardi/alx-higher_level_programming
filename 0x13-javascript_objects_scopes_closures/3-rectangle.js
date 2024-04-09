d919dbe3284398b861a1ebcbf0d9b4c9a9fd3368
simowardi <daoist32@gmail@gmail.com>
2024-04-09 16:27:46 -0700

    javascript basics

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,12 +4,7 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (this.width === 0 || this.height === 0) {
-      return;
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-}
+};
