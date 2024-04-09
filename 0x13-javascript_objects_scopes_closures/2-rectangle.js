d3f61cacad6b92c5583a14d37b7fec23b1883a2e
simowardi <daoist32@gmail@gmail.com>
2024-04-09 16:24:39 -0700

    javascript basics

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      this.width = 0;
-      this.height = 0;
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
+};
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      this.width = 0;
-      this.height = 0;
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print() {
@@ -20,5 +13,3 @@ class Rectangle {
     }
   }
 }
-
-module.exports = Rectangle;
