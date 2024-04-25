fc11cf2c78e440db5133b4e781fdac04eecfb6fe
simowardi <daoist32@gmail@gmail.com>
2024-04-25 10:40:12 -0700

    task 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if the URL is provided as an argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-# Store the URL in a variable
-url="$1"
-
-# Send the GET request using curl and get the response body and status code
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-status_code=${response: -3}
-body=$(curl -s "$url")
-
-# Check if the status code is 200 and print the body
-if [ "$status_code" = "200" ]; then
-    echo "$body"
-fi
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Send a DELETE request to a URL and display the response body.
+curl -sX DELETE "$1"
