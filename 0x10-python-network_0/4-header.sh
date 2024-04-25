99ac5a5ec52f72b1aeefcdf73a5daca1b1e27e92
simowardi <daoist32@gmail@gmail.com>
2024-04-25 11:08:21 -0700

    updt task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with header var 'X-School-User-Id' with var 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# sends a GET request with header variable being 'X-School-User-Id' with variable 98
+curl -sH "X-School-User-Id: 98" "$1"
