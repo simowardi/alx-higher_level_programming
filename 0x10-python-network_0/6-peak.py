ad5109315e8910dd7b2c2ab5749a9bdac8405965
simowardi <daoist32@gmail@gmail.com>
2024-04-25 11:52:27 -0700

    updt task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,6 @@
 def find_peak(list_of_integers):
     """
     Finds a peak element in a list of unsorted integers.
-    A peak element is an element that is
-    greater than or equal to its neighbors.
     """
     if not list_of_integers or list_of_integers == []:
         return None
