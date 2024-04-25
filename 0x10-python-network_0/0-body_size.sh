6d853a1e4cde3593cec6aa078b0c561896f1ee35
simowardi <daoist32@gmail@gmail.com>
2024-04-25 10:03:54 -0700

    updt 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# This script sends a HEAD request to a given URL using curl
-# and retrieves the size of the response body in bytes.
-
-# Check if the URL is provided as an argument
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-# Store the URL in a variable
-url="$1"
-
-# Send the request using curl and get the response body size
-body_size=$(curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}' | tr -d '\r')
-
-# Print body size
-echo "$body_size"
+# Get the byte size of the HTTP response header for a given URL.
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
