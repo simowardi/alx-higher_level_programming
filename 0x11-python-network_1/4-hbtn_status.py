e246c0bcc711ef83a34e92d0a796d1655cb94071
simowardi <daoist32@gmail@gmail.com>
2024-04-26 11:52:33 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This script fetches the URL https://alx-intranet.hbtn.io/status
+and displays the body of the response.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    # Define the URL
+    url = "https://alx-intranet.hbtn.io/status"
+
+    # Send the GET request
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
