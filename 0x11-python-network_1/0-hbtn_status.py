65d91e1ee0a11ec14ab5b34eef5e1a73534b456d
simowardi <daoist32@gmail@gmail.com>
2024-04-26 10:42:22 -0700

    task 0 update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,24 +2,13 @@
 import urllib.request
 
 
-def fetch_url(url):
-    """Fetches the status from a given URL using urllib.
-
-    Args: url (str): The URL to fetch the status from.
-
-    Returns: str: The body response content.
-    """
-    with urllib.request.urlopen(url) as responde:
-        content = responde.read()
-
-    return content
-
-
 if __name__ == '__main__':
+
     url = "https://alx-intranet.hbtn.io/status"
 
     # Fetch the status content from the URL
-    status = fetch_url(url)
+    with urllib.request.urlopen(url) as response:
+        status = response.read()
 
     print("Body response:")
     print(f"\t- type: {type(status)}")
