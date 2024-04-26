980a59d1d2dd2f035cf1d363610233ef0a81e697
simowardi <daoist32@gmail@gmail.com>
2024-04-26 11:58:07 -0700

    task 3 updt

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,17 +4,19 @@ This script sends a request to a specified URL and
 displays the body of the response.
 """
 import sys
-import urllib.request
 import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
 
     url = sys.argv[1]       # Extract the URL from the command line argument
 
+    req_obj = urllib.request.Request(url)
+
     try:
         # Send the request and open the response
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(req_obj) as response:
             # Read and decode the response body in utf-8
             response_body = response.read().decode('ancii')
 
