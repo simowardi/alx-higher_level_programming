45d1cae39ec5fa99e2a9e12b071a254f5b180ffe
simowardi <daoist32@gmail@gmail.com>
2024-04-26 12:05:24 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+This script sends a POST request to the specified URL
+with the email as a parameter and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Extract the URL from the command-line argument
+    url = sys.argv[1]
+
+    # Extract the email from the command-line argument
+    email = sys.argv[2]
+
+    # Send the POST request with email as a parameter
+    response = requests.post(url, data={'email': email})
+
+    # Display the body of the response
+    print(response.text)
