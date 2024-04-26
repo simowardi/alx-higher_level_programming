2a9f0abb41a6aaaed3bce637def12e1eb33bdf7a
simowardi <daoist32@gmail@gmail.com>
2024-04-26 12:13:34 -0700

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+This script takes in a letter,
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter,
+and then displays the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Extract the letter command-line argument
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    # Send a POST request with the letter as a parameter
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        # Extract and decode the JSON response
+        result = response.json()
+
+        # Check if the JSON is empty
+        if len(result) == 0:
+            print("No result")
+
+        # Check if the JSON is not empty and properly formatted
+        elif 'id' in result and 'name' in result:
+            print("[%d] %s" % (result['id'], result['name']))
+
+    # Handle invalid JSON formatting
+    except ValueError:
+        print("Not a valid JSON")
