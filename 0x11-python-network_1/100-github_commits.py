be42dfd24364258b19cd548886476dc931f7da82
simowardi <daoist32@gmail@gmail.com>
2024-04-26 12:33:06 -0700

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,16 +15,16 @@ if __name__ == "__main__":
     # Get the repository and owner names from the command line arguments
     repo = sys.argv[1]
     owner = sys.argv[2]
-    
+ 
     # Construct the API URL using the repository and owner names
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    
+
     # Make a GET request to the GitHub API
     response = requests.get(url)
 
     if response.status_code == 200:
         commits = response.json()[:10] # Get the most recent 10 commits
-        
+
         for commit in commits:
             sha = commit['sha']
             author = commit['commit']['author']['name']
