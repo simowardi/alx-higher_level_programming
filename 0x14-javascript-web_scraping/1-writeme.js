1120eefedf0fb9540a5e580d051afb500e394150
simowardi <daoist32@gmail@gmail.com>
2024-05-21 14:32:50 -0700

    JS web scraping updt 1&5

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,14 +3,14 @@
 
 const fs = require('fs');
 
-// Get the file path and string to write from command line arguments
 const filePath = process.argv[2];
+
 const stringToWrite = process.argv[3];
 
 // Write the string to the file in utf-8 encoding
 fs.writeFile(filePath, stringToWrite, 'utf8', (error) => {
   if (error) {
-    console.error(error);
+    console.log(error);
     return;
   }
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // script that gets the contents of a webpage and stores it in a file
 
+const apiUrl = process.argv[2];
+
+const stringToWrite = process.argv[3];
 
 // Import the built-in Node.js 'fs' module
 const fs = require('fs');
 
-// Import the 'request' module
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
 // Use the 'request' module to perform an HTTP GET request to the URL
-request(apiUrl).pipe(fs.createWriteStream(process.argv[3]));
+request(apiUrl).pipe(fs.createWriteStream(stringToWrite));
