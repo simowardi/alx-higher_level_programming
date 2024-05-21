565a12cb96ad41902eccf360f2823239f79eb549
simowardi <daoist32@gmail@gmail.com>
2024-05-21 14:24:41 -0700

    JS web scraping 5&6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file
+
+
+// Import the built-in Node.js 'fs' module
+const fs = require('fs');
+
+// Import the 'request' module
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+// Use the 'request' module to perform an HTTP GET request to the URL
+request(apiUrl).pipe(fs.createWriteStream(process.argv[3]));
