4593dad6532161353a6cd077d42a258ffb5f13d6
simowardi <daoist32@gmail@gmail.com>
2024-05-21 14:01:52 -0700

    JS web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -20,12 +20,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-// Check if both arguments are provided
-if (!filePath || !stringToWrite) {
-  console.error('Usage: ./1-writeme.js <file-path> <string-to-write>');
-  process.exit(1);
-}
-
 // Write the string to the file in utf-8 encoding
 fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
