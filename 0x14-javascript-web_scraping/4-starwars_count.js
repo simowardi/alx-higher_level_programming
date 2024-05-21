0900b81759bf64a32daa55639e27a2a2468ccbb6
simowardi <daoist32@gmail@gmail.com>
2024-05-21 14:11:02 -0700

    JS web scraping 3&4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+// This script prints the number of movies where the character "Wedge Antilles" (character ID 18) is present.
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  // Make a GET request to the Star Wars API.
+
+  if (error) {
+    // Print the error object if an error occurred.
+    console.error(error);
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+
+  // Filter films where Wedge Antilles (character ID 18) is present
+  const count = films.filter(film => film.characters.includes(`${apiUrl}${characterId}/`)).length;
+
+  console.log(count)
+});
